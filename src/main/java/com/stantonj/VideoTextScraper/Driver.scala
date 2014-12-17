a83286a00cccf79ea2f4bf4a9f65ea97f27ed779
Joe Stanton <jsromejoe@gmail.com>
2014-12-16 23:40:29 -0500

    added command line arguments added call to VideoTextScraper in main completed initial setup of frame extraction loop

--- src/main/java/com/stantonj/VideoTextScraper/Driver.scala
+++ src/main/java/com/stantonj/VideoTextScraper/Driver.scala
@@ -1,8 +1,82 @@
 package com.stantonj.VideoTextScraper
 
+import java.awt.Rectangle
+import java.io.File
+
+import org.clapper.argot.{ArgotConversionException, ArgotParser, ArgotUsageException}
+
 object Driver {
 
-  def main (args: Array[String]) {
+  val parser = new ArgotParser(
+    "VideoTextScraper",
+    preUsage = Some("VideoTextScraper: Version 0.1. Copyright (c) " +
+      "2014, Joe Stanton.")
+  )
 
+  val TimeOffset = parser.option[Double](List("s", "StartTime"), "t", "Time to start Scraping") {
+    (sValue, opt) =>
+
+      try {
+        require(sValue.toDouble >= 0.0)
+        sValue.toDouble
+      }
+
+      catch {
+        case _: NumberFormatException =>
+          throw new ArgotConversionException(
+            "Option " + opt.name + ": \"" + sValue + "\" isn't a valid start time."
+          )
+      }
+  }
+
+  val TimeStep = parser.option[Double](List("t", "TimeStep"), "t", "Size of time step to use") {
+    (sValue, opt) =>
+
+      try {
+        require(sValue.toDouble > 0.0)
+        sValue.toDouble
+      }
+
+      catch {
+        case _: NumberFormatException =>
+          throw new ArgotConversionException(
+            "Option " + opt.name + ": \"" + sValue + "\" isn't a valid time step."
+          )
+      }
+  }
+
+  val input = parser.parameter[String]("inputfile",
+    "Video file to scrape",
+    false) {
+    (s, opt) =>
+      val file = new File(s)
+      if (!file.exists)
+        parser.usage("Input file \"" + s + "\" does not exist.")
+
+      s
+  }
+
+  val areas = parser.multiParameter[Rectangle]("aoi",
+    "Areas of interest to scrape (x,y,w,h)",
+    false) {
+    (s: String, opt) =>
+      val _parts = s.replace("(", " ").replace(")", " ").replace(",", " ").split("[ \\t]+").toList //.map(_.toInt)
+    val parts = _parts.filter(_ != "").map(_.toInt)
+      new Rectangle(parts(0), parts(1), parts(2), parts(3))
+  }
+
+  def main (args: Array[String]) {
+    try {
+      parser.parse(args)
+      val offset = TimeOffset.value.getOrElse(0.0)
+      val step = TimeStep.value.getOrElse(1.0)
+      val inputFilePath = input.value.get
+
+      val Scraper = new VideoTextScraper(inputFilePath, areas.value.toList, step, offset)
+      Scraper.persistFrames()
+    }
+    catch {
+      case e: ArgotUsageException => println(e.message)
+    }
   }
 }
--- src/main/java/com/stantonj/VideoTextScraper/VideoTextScraper.scala
+++ src/main/java/com/stantonj/VideoTextScraper/VideoTextScraper.scala
@@ -1,39 +1,51 @@
 package com.stantonj.VideoTextScraper
 
 import java.awt.image.BufferedImage
-import java.awt.{Polygon, Rectangle}
-import java.io.FileInputStream
+import java.awt.{Rectangle, Shape}
+import java.io.{File, FileInputStream}
 import java.nio.MappedByteBuffer
 import java.nio.channels.FileChannel
-import java.util
+import javax.imageio.ImageIO
 
-import org.jcodec.api.FrameGrab
+import org.jcodec.api.{FrameGrab, JCodecException}
 import org.jcodec.common.ByteBufferSeekableByteChannel
 
-class VideoTextScraper(VideoFilePath: String, AOI: List[Polygon], TimeStep: Double = 1.0, TimeOffset: Double = 0.0) {
+import scala.collection.mutable
 
-  val (frames) = {
+class VideoTextScraper(VideoFilePath: String, AOI: List[Shape], TimeStep: Double = 1.0, TimeOffset: Double = 0.0) {
+
+  val (frames: mutable.HashMap[Double, List[BufferedImage]]) = {
     val videoInputStream = new FileInputStream(VideoFilePath)
     val videoFileChannel = videoInputStream.getChannel()
     val videoByteBuffer: MappedByteBuffer = videoFileChannel.map(FileChannel.MapMode.READ_ONLY, 0L, videoFileChannel.size())
     var grab: FrameGrab = new FrameGrab(new ByteBufferSeekableByteChannel(videoByteBuffer))
-    val frames = new util.HashMap[Double, List[BufferedImage]]()
+    val frames = new mutable.HashMap[Double, List[BufferedImage]]()
     var TimeStamp: Double = TimeOffset
 
     val Areas: List[Rectangle] = AOI.map(_.getBounds())
 
-    grab = grab.seek(TimeStamp)
-    while (grab != null) {
+    grab = grab.seekToSecondPrecise(TimeStamp)
+    var done = false
+    while (!done) {
       val frame: BufferedImage = grab.getFrame()
 
       frames.put(TimeStamp, Areas.map(a => frame.getSubimage(a.getX().toInt, a.getY().toInt, a.getWidth().toInt, a.getHeight().toInt)))
-
+      //println(TimeStamp)
       TimeStamp += TimeStep
-      grab = grab.seek(TimeStamp)
+      //println(TimeStamp)
+      try {
+        grab = grab.seekToSecondSloppy(TimeStamp)
+      }
+      catch {
+        case je: JCodecException => done = true
+        case e: Exception => System.exit(-1)
+      }
+      //grab.seek(TimeStamp)//grab = grab.seek(TimeStamp)
     }
 
     (frames)
   }
 
+  def persistFrames(): Unit = frames.foreach(p => p._2.zipWithIndex.foreach(a => ImageIO.write(a._1, "png", new File(VideoFilePath + "_" + p._1.toString() + "_" + a._2.toString() + ".png"))))
 
 }
